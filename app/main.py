d313fa21f198fe1d9352e09712925d2c2a70b049
Hojun An <pockguy96@gmail.com>
2025-11-19 11:02:33 -0500

    feat: break the connection with entire database.(2)

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 # from app.db.init import database
-from app.route import auth, db
+# from app.route import auth, db
 from app.route import test
 from app.firebase.init import initialize_firebase
 import os
