8e2ed79468c0aac604a767b6c5838adeb446145c
Hojun An <pockguy96@gmail.com>
2025-11-19 10:50:44 -0500

    feat: make a disable to some functions in purpose.

--- app/main.py
+++ app/main.py
@@ -12,8 +12,8 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-FRONT_URL = os.getenv("FRONT_URL")
-BACK_URL = os.getenv("BACK_URL")
+# FRONT_URL = os.getenv("FRONT_URL")
+# BACK_URL = os.getenv("BACK_URL")
 
 # Create FastAPI instance
 app = FastAPI()
@@ -21,7 +21,7 @@ app = FastAPI()
 # CORS configuration to allow the specified frontend URL
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONT_URL, BACK_URL],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
@@ -31,21 +31,21 @@ app.add_middleware(
 initialize_firebase()
 
 # Connect to the database on startup
-@app.on_event("startup")
-async def startup():
-    print("Connecting to the database")
-    await database.connect()
+# @app.on_event("startup")
+# async def startup():
+#     print("Connecting to the database")
+#     await database.connect()
 
 # # # Disconnect from the database on shutdown
-@app.on_event("shutdown")
-async def shutdown():
-    print("Disconnecting from the database")
-    await database.disconnect()
+# @app.on_event("shutdown")
+# async def shutdown():
+#     print("Disconnecting from the database")
+#     await database.disconnect()
 
 # Register routes
-app.include_router(db.router, prefix="/db")
-app.include_router(auth.router, prefix="/auth")
-app.include_router(test.router, prefix="/test")
+# app.include_router(db.router, prefix="/db")
+# app.include_router(auth.router, prefix="/auth")
+# app.include_router(test.router, prefix="/test")
 
 @app.get("/")
 async def root():
