61226f526bd7e4718a9677d7c5ea280de94d1130
Hojun An <pockguy96@gmail.com>
2025-11-21 14:31:11 -0500

    feat: get to origin db/init.py.

--- app/db/init.py
+++ app/db/init.py
@@ -1,43 +1,28 @@
-from sqlalchemy import create_engine
-from app.db.model import Base
-
-# sync 드라이버 사용 (중요!!)
-DATABASE_URL = "postgresql+psycopg2://ftree_user:Jsky4987!!@/postgres?host=/cloudsql/finance-tree:northamerica-northeast1:ftree-db"
-
-engine = create_engine(DATABASE_URL)
-
-print("Creating tables...")
-Base.metadata.create_all(bind=engine)
-print("DONE")
-
-
 # app/db/init.py
 
-# import os
-# from sqlalchemy import create_engine, MetaData
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-# from databases import Database
-# from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from databases import Database
+from dotenv import load_dotenv
 
-# # Load environment variables from .env file
-# load_dotenv()
+# Load environment variables from .env file
+load_dotenv()
 
-# # Read the database URL from environment variables
-# # DATABASE_URL = os.getenv("DATABASE_URL")
-# DATABASE_URL="postgresql+asyncpg://ftree_user:Jsky4987!!@/postgres?host=/cloudsql/finance-tree:northamerica-northeast1:ftree-db"	
+# Read the database URL from environment variables
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Set up SQLAlchemy engine and metadata
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+Base = declarative_base()
 
-# # Set up SQLAlchemy engine and metadata
-# engine = create_engine(DATABASE_URL)
-# metadata = MetaData()
-# Base = declarative_base()
+# Create a Database object for async operations
+database = Database(DATABASE_URL, ssl=True)
 
-# # Create a Database object for async operations
-# database = Database(DATABASE_URL, ssl=True)
+# SQLAlchemy session setup for synchronous database interactions
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# # SQLAlchemy session setup for synchronous database interactions
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# # Create all database tables if they don't exist
+# Create all database tables if they don't exist
 # Base.metadata.create_all(bind=engine)
--- app/main.py
+++ app/main.py
@@ -58,7 +58,7 @@ app.include_router(test.router, prefix="/test")
 @app.get("/")
 async def root():
     return {
-        "message": "Version Code - 32",
+        "message": "Version Code - 33",
         "FRONT": FRONT_URL,
         "BACK": BACK_URL,
     }
