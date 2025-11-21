58e26e728416e5e62bf3b27cd7947f3bdf926682
Hojun An <pockguy96@gmail.com>
2025-11-21 12:07:40 -0500

    feat: code auth.py/verify-email(post) -> do not send email actually..(2)

--- app/main.py
+++ app/main.py
@@ -58,7 +58,7 @@ app.include_router(test.router, prefix="/test")
 @app.get("/")
 async def root():
     return {
-        "message": "Version Code - 31",
+        "message": "Version Code - 32",
         "FRONT": FRONT_URL,
         "BACK": BACK_URL,
     }
